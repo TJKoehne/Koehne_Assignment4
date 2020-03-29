0e6d018d63ac8668af044b9894affe9f68faba50
unknown <thomaskoehnej@gmail.com>
2020-03-29 17:24:32 -0400

    Working Solution for First Problem

--- Koehne_Problem1/src/problem1/Application.java
+++ Koehne_Problem1/src/problem1/Application.java
@@ -2,14 +2,17 @@ package problem1;
 
 public class Application {
 
-	static String INPUT_FILE_PATH = new String("problem1.txt");
-	static String OUTPUT_FILE_PATH = new String("unique_words.txt");
+	static String INPUT_FILE_NAME = new String("problem1.txt");
+	static String OUTPUT_FILE_NAME = new String("unique_words.txt");
 	
 	public static void main(String[] args) {
-		DuplicateRemover dr1 = new DuplicateRemover();
-		dr1.remove(INPUT_FILE_PATH);
-		dr1.write(OUTPUT_FILE_PATH);
 		
+		DuplicateRemover dr1 = new DuplicateRemover();
+		
+		dr1.remove(INPUT_FILE_NAME);
+		dr1.write(OUTPUT_FILE_NAME);
+		
+		System.out.println("Application Complete: Unique Words Generated.");
 	}
 
 }
--- Koehne_Problem1/src/problem1/DuplicateRemover.java
+++ Koehne_Problem1/src/problem1/DuplicateRemover.java
@@ -2,39 +2,51 @@ package problem1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DuplicateRemover {
 
+	//The Two array list containing all the information from the input
+	//and the information going to the output
 	ArrayList<String> fileContent = new ArrayList<String>();
+	ArrayList<String> uniqueWords = new ArrayList<String>();
 	
 	public void remove (String dataFile) {
+		
 		File inputFile = new File(dataFile);
+		
+		//Try catch that imports the text from a file into an ArrayList
 		try (Scanner input = new Scanner(inputFile)){
 			while(input.hasNext()) {
-				fileContent.add(input.next());
+				fileContent.add(input.next().toLowerCase().replaceAll("\\p{Punct}", ""));
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found please try again");
-		} 
+		}
 		
+		//For loop checking if a word is unique before adding it to the output array list
 		for(int i = 0; i < fileContent.size(); i++) {
-			System.out.print(fileContent.get(i) + "\n");
-			
+			if (!uniqueWords.contains(fileContent.get(i))) {
+				uniqueWords.add(fileContent.get(i));
+			}
 		}
 	}
 	
 	public void write (String outputFile) {
 		
+		File fileOutput = new File(outputFile);
+		
+		//Try catch that writes the unique words to output file
+		try (PrintWriter output = new PrintWriter(outputFile);) {
+			for (int i = 0; i < uniqueWords.size(); i++) {
+				output.println(uniqueWords.get(i));
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found please try again");
+		}
 	}
 }
 
-/* 
- * if (inputFile.isFile()) {
-			System.out.println("file exist");
-		} else {
-			System.out.println("file doesn't exist");
-		}
- * 
- */
+
